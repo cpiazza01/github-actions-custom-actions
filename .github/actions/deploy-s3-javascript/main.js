3da3333a1d9f70a2ce28a2df5ba5efe918d7fa14
Cody Piazza <codypiazza@gmail.com>
2024-05-27 14:21:23 -0500

    fix url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,7 @@ function run() {
   const s3URI = `s3://${bucket}`
   exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
 
-  const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  const websiteURL = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`
   core.setOutput('website-url', websiteURL)
 }
 
